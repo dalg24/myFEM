a2817eac3100f4f0238085f26c9703482246b2c3
Damien Lebrun-Grandie <dalg24@gmail.com>
2011-07-14 09:21:22 -0500

    ca avance

--- test.cpp
+++ test.cpp
@@ -29,15 +29,17 @@ public:
   QuadratureRule(const QuadratureRule& qr) : points(qr.points), weights(qr.weights) { }
   QuadratureRule& operator=(const QuadratureRule& qr) { if (this==&qr) return *this; points = qr.points; weights = qr.weights; return *this; }
 
-  unsigned int getNumberQuadraturePoints() const { assert(points.size()==weights.size()); return points.size(); }
-  Point getQuadraturePoint(unsigned int i) const { return points[i]; }
+  unsigned int getNumberOfQuadraturePoints() const { assert(points.size()==weights.size()); return points.size(); }
+  Point getQuadraturePoint(unsigned int iqp) const { return points[iqp]; }
   std::vector<Point> getQuadraturePoints() const { return points; }
-  double getWeight(unsigned int i) const { return weights[i]; }
+  double getWeight(unsigned int iqp) const { return weights[iqp]; }
   std::vector<double> getWeights() const { return weights; }
+  std::vector<Point> getSupportPoints() const { return sp; }
 
 protected:
   std::vector<Point> points;
   std::vector<double> weights;
+  std::vector<Point> sp;
 }; // end class QuadratureRule
 
 class GaussianTwoPoints : public QuadratureRule {
@@ -45,6 +47,7 @@ public:
   GaussianTwoPoints() {
     points.push_back(Point(-1.0 / sqrt(3.0))); weights.push_back(1.0);
     points.push_back(Point(1.0 / sqrt(3.0))); weights.push_back(1.0);
+    sp.push_back(Point(-1.0)); sp.push_back(Point(1.0));
   }
 }; // end class GaussianTwoPoints
 
@@ -54,6 +57,7 @@ public:
     points.push_back(Point(-sqrt(3.0 / 5.0))); weights.push_back(5.0 / 9.0);
     points.push_back(Point(0.0)); weights.push_back(8.0 / 9.0);
     points.push_back(Point(sqrt(3.0 / 5.0))); weights.push_back(5.0 / 9.0);
+    sp.push_back(Point(-1.0)); sp.push_back(Point(1.0));
   }
 }; // end class GaussianThreePoints
 
@@ -66,12 +70,14 @@ public:
   BasisFunctions& operator=(const BasisFunctions& bf) { if (this==&bf) return *this; nodes = bf.nodes; return *this; }
 
 
-  unsigned int getNumberNodes() const { return nodes.size(); }
-  Point getNode(unsigned int i) const { return nodes[i]; }
+  unsigned int getNumberOfNodes() const { return nodes.size(); }
+  Point getNode(unsigned int idof) const { return nodes[idof]; }
   std::vector<Point> getNodes() const { return nodes; }
 
-  virtual double val(unsigned int, Point) const = 0;
-  virtual double dx(unsigned int, Point) const = 0;
+  std::vector<Point> getSupportPoints() const { std::vector<Point> sp; sp.push_back(nodes[0]); sp.push_back(nodes[1]); return sp;}
+
+  virtual double getVal(unsigned int, Point) const = 0;
+  virtual double getDx(unsigned int, Point) const = 0;
 
 protected:
   std::vector<Point> nodes;
@@ -81,12 +87,12 @@ class PiecewiseLinear : public BasisFunctions {
 public:             
   PiecewiseLinear(std::vector<Point> sp) { nodes = sp; }
 
-  double val(unsigned int i, Point p) const {
+  double getVal(unsigned int idof, Point p) const {
     assert(nodes[0].x <= p.x); assert(p.x <= nodes[1].x);
     double value;
-    if (i == 0) {
+    if (idof == 0) {
       value = (nodes[1].x - p.x) / (nodes[1].x - nodes[0].x); 
-    } else if (i == 1) {
+    } else if (idof == 1) {
       value = (p.x - nodes[0].x) / (nodes[1].x - nodes[0].x); 
     } else {
       std::cerr<<"aie"<<std::endl;
@@ -95,12 +101,12 @@ public:
     return value;
   }
 
-  double dx(unsigned int i, Point p) const {
+  double getDx(unsigned int idof, Point p) const {
     assert(nodes[0].x <= p.x); assert(p.x <= nodes[1].x);
     double value;
-    if (i == 0) {
+    if (idof == 0) {
       value = -1.0 / (nodes[1].x - nodes[0].x); 
-    } else if (i == 1) {
+    } else if (idof == 1) {
       value = 1.0 / (nodes[1].x - nodes[0].x); 
     } else {
       std::cerr<<"aie"<<std::endl;
@@ -114,14 +120,14 @@ class PiecewiseQuadratic : public BasisFunctions {
 public:             
   PiecewiseQuadratic(std::vector<Point> sp) { nodes = sp; nodes.push_back((sp[0] + sp[1]) / 2.0); }
 
-  double val(unsigned int i, Point p) const {
+  double getVal(unsigned int idof, Point p) const {
     assert(nodes[0].x <= p.x); assert(p.x <= nodes[1].x);
     double value;
-    if (i == 0) {
+    if (idof == 0) {
       value = (nodes[1].x - p.x) * (nodes[2].x - p.x) / ((nodes[1].x - nodes[0].x) * (nodes[2].x - nodes[0].x)); 
-    } else if (i == 1) {
+    } else if (idof == 1) {
       value = (nodes[2].x - p.x) * (nodes[0].x - p.x) / ((nodes[2].x - nodes[1].x) * (nodes[0].x - nodes[1].x)); 
-    } else if (i == 2) {
+    } else if (idof == 2) {
       value = (nodes[0].x - p.x) * (nodes[1].x - p.x) / ((nodes[0].x - nodes[2].x) * (nodes[1].x - nodes[2].x)); 
     } else {
       std::cerr<<"aie"<<std::endl;
@@ -130,14 +136,14 @@ public:
     return value;
   }
 
-  double dx(unsigned int i, Point p) const {
+  double getDx(unsigned int idof, Point p) const {
     assert(nodes[0].x <= p.x); assert(p.x <= nodes[1].x);
     double value;
-    if (i == 0) {
+    if (idof == 0) {
       value = (2.0 * p.x - nodes[1].x - nodes[2].x) / ((nodes[1].x - nodes[0].x) * (nodes[2].x - nodes[0].x)); 
-    } else if (i == 1) {
+    } else if (idof == 1) {
       value = (2.0 * p.x - nodes[2].x - nodes[0].x) / ((nodes[2].x - nodes[1].x) * (nodes[0].x - nodes[1].x)); 
-    } else if (i == 2) {
+    } else if (idof == 2) {
       value = (2.0 * p.x - nodes[0].x - nodes[1].x) / ((nodes[0].x - nodes[2].x) * (nodes[1].x - nodes[2].x)); 
     } else {
       std::cerr<<"aie"<<std::endl;
@@ -165,15 +171,17 @@ public:
   }
   ~ReferenceElement() { delete bf; }
 
-  unsigned int getNumberNodes() const { return bf->getNumberNodes(); }
-  Point getNode(unsigned int i) const { return bf->getNode(i); }
+  unsigned int getNumberOfNodes() const { return bf->getNumberOfNodes(); }
+  Point getNode(unsigned int idof) const { return bf->getNode(idof); }
   std::vector<Point> getNodes() const { return bf->getNodes(); }
   std::vector<Point> getSupportPoints() const { return sp; }
+  double getVal(unsigned int idof, Point p) const { return bf->getVal(idof, p); }
+  double getDx(unsigned int idof, Point p) const { return bf->getDx(idof, p); }
 
 protected:
   std::vector<Point> sp; 
   BasisFunctions *bf;
-};
+}; // end class ReferenceElement
 
 ////////////////////////// FINITE ELEMENT //////////////////////////////////////
 class FiniteElement {
@@ -185,27 +193,77 @@ public:
     }
   }
 
-  Point mapGlobalToLocal(Point g) {
+  Point mapGlobalToLocal(Point g) const {
     std::vector<Point> ref_sp = re->getSupportPoints();
     assert(sp[0].x <= g.x); assert(g.x <= sp[1].x);
     Point l;
     l.x = ref_sp[0].x + (ref_sp[1].x - ref_sp[0].x) * (g.x - sp[0].x) / (sp[1].x - sp[0].x);
     return l;
   }
-
-  Point mapLocalToGlobal(Point l) {
+  Point mapLocalToGlobal(Point l) const {
     std::vector<Point> ref_sp = re->getSupportPoints();
     assert(ref_sp[0].x <= l.x); assert(l.x <= ref_sp[1].x);
     Point g;
     g.x = sp[0].x + (sp[1].x - sp[0].x) * (l.x - ref_sp[0].x) / (ref_sp[1].x - ref_sp[0].x);
     return g;
   }
+  double getValByLocal(unsigned int idof, Point l) const { return re->getVal(idof, l); }
+  double getValByGlobal(unsigned int idof, Point g) const { Point l = mapGlobalToLocal(g); return re->getVal(idof, l); }
+  double getDxByLocal(unsigned int idof, Point l) const { return re->getDx(idof, l); }
+  double getDxByGlobal(unsigned int idof, Point g) const { Point l = mapGlobalToLocal(g); return re->getDx(idof, l); }
+
+  unsigned int getNumberOfNodes() const { return re->getNumberOfNodes(); }
 
 protected:
   std::vector<Point> sp;
   ReferenceElement *re;
-};
+}; // end class FiniteElement
 
+////////////////////////// FE Values //////////////////////////////////////
+class UpdateFlags { };
+class FEValues {
+public:
+  FEValues(FiniteElement *e, QuadratureRule *r, UpdateFlags f) : fe(e), qr(r), uf(f) { } 
+
+  void reinit() {
+    Weights = qr->getWeights();
+    const unsigned int nqp = qr->getNumberOfQuadraturePoints();
+    const unsigned int ndof = fe->getNumberOfNodes();
+    ShapeValues.resize(ndof);
+    ShapeDx.resize(ndof);
+    for (unsigned int idof = 0; idof < ndof; ++idof) {
+      ShapeValues[idof].resize(nqp);
+      ShapeDx[idof].resize(nqp);
+      for (unsigned int iqp = 0; iqp < nqp; ++iqp) {
+        ShapeValues[idof][iqp] = fe->getValByLocal(idof, qr->getQuadraturePoint(iqp));
+        ShapeDx[idof][iqp] = fe->getDxByLocal(idof, qr->getQuadraturePoint(iqp));
+      } 
+    }
+
+  }
+
+  double getDeterminantOfJacobianTimesWeight(unsigned int iqp) const { return Weights[iqp]*DeterminantOfJacobian; }
+  double getShapeValue(unsigned int idof, unsigned int iqp) const { return ShapeValues[idof][iqp]; }
+  double getShapeDx(unsigned int idof, unsigned int iqp) const { return ShapeDx[idof][iqp]; }
+  std::vector<Point> getQuadraturePoints() const {
+    std::vector<Point> qps;
+    const unsigned int nqp = qr->getNumberOfQuadraturePoints();
+    for (unsigned int iqp = 0; iqp < nqp; ++iqp) {
+      qps.push_back(fe->mapLocalToGlobal(qr->getQuadraturePoint(iqp)));
+    }
+    return qps;
+  }
+
+protected:
+  std::vector<double> Weights;
+  double DeterminantOfJacobian;
+  std::vector<std::vector<double> > ShapeValues;
+  std::vector<std::vector<double> > ShapeDx;
+
+  FiniteElement *fe;
+  QuadratureRule *qr;
+  UpdateFlags uf;
+}; // end class FEValues
 
 int main(int argc, char *argv[]) {
   ReferenceElement *referenceElement = new ReferenceElement;
@@ -231,7 +289,7 @@ int main(int argc, char *argv[]) {
   //bf = new PiecewiseLinear(dummySupportPoints);
   qr = new GaussianThreePoints;
   bf = new PiecewiseQuadratic(dummySupportPoints);
-  unsigned int n_qp = qr->getNumberQuadraturePoints();
+  unsigned int n_qp = qr->getNumberOfQuadraturePoints();
   std::vector<Point> qp = qr->getQuadraturePoints();
 
   std::cout<<"points regulary spaced\n";
@@ -242,8 +300,8 @@ int main(int argc, char *argv[]) {
     qp.push_back(Point(-1.0+i*2.0/double(n_qp-1)));
   for (unsigned int i = 0; i < n_qp; ++i) {
     std::cout<<"x="<<qp[i].x<<"  ";
-    for (unsigned int j = 0; j < bf->getNumberNodes(); ++j) {
-      std::cout<<"phi_"<<j<<"="<<bf->val(j, qp[j])<<"  ";
+    for (unsigned int j = 0; j < bf->getNumberOfNodes(); ++j) {
+      std::cout<<"phi_"<<j<<"="<<bf->getVal(j, qp[j])<<"  ";
     }
     std::cout<<"\n";
   }
